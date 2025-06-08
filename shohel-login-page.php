8950021f2caf0617f2f84ff8850f67d3a4309e32
MD. Shohel Rana <shohel065931@gmail.com>
2025-06-08 08:38:30 +0600

    tyry

---
+++ shohel-login-page.php
@@ -0,0 +1,21 @@
+<?php  
+
+/*
+* Plugin Name: Shohel Login Page
+* Plugin URI: https://wordpress.org/plugins/shohel-login-page
+* Description: A simple plugin to customize the WordPress login page with a custom logo and background.
+* Version: 1.0.0
+* Requires at least: 5.8
+* Requires PHP: 7.0
+* Author: Shohel Rana
+* Author URI: https://shohelrana.top
+* License: GPLv2 or later 
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain: shohel-login-page
+*/
+
+
+
+
+
+?>
